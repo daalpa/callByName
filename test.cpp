22921b41c242be0878da6e6bcdb0e7fba748f7a4
daalpa <daalpa@gmail.com>
2015-08-15 17:20:15 +0100

    Improved the test

--- test.cpp
+++ test.cpp
@@ -6,15 +6,28 @@
 #include <iostream>
 #include "callByName.h"
 
-FUNCTION_CALL_BY_NAME(AAAA) {
-    std::cout << "Called AAAA\n";
+FUNCTION_CALL_BY_NAME(first_function) {
+    std::cout << "Called first_function\n";
 }
 
-FUNCTION_CALL_BY_NAME(BBBB) {
-    std::cout << "Called BBBB\n";
+FUNCTION_CALL_BY_NAME(second_function) {
+    std::cout << "Called second_function\n";
 }
 
 int main(int argc, char* argv[]) {
-    callFunction(argv[1]);
+    if(argc > 1) {
+        try {
+        callFunction(argv[1]);
+        } catch(std::out_of_range& ex) {
+            std::cerr << "The function " << argv[1] << " does not exist." << '\n';
+        }
+    }
+    else {
+        std::cout << "No function to call provided. Type: " << argv[0] << " FUNCTION_NAME \n"
+                     "where FUNCTION_NAME is any of the following\n";
+        for(const auto& name_function : functionContainer) {
+            std::cout << '\t' << name_function.first << '\n';
+        }
+    }
     return 0;
 }
